dd7889378a8250f2393b25d0c8e090aa2fbfc2ba
TheEletricboy <54644862+TheEletricboy@users.noreply.github.com>
2019-09-25 15:52:14 +0100

    Bootstrap Intro

---
+++ index.js
@@ -0,0 +1,10 @@
+/*WE IS GONNA WRITE SOME INCREDIBLE NODE*/
+
+console.log('this is a not applicationa# and next week we are gonna build an amazing game')
+
+var i = 0;
+while(true){
+	i++
+	console.log(i)
+	if(i>=10)break
+}
